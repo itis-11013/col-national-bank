f862c8e1c10efa93da73a6a41070f188b0fbf4b1
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-05-14 13:56:21 +0300

    security 2

--- sem_col/src/main/java/ru/itis/sem_col/config/WebSecurityConfig.java
+++ sem_col/src/main/java/ru/itis/sem_col/config/WebSecurityConfig.java
@@ -1,4 +1,50 @@
 package ru.itis.sem_col.config;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.itis.sem_col.security.OrganizationDetailService;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    @Qualifier("myOrganizationDetailService")
+    private OrganizationDetailService myOrganizationDetailService;
+
+    @Autowired
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myOrganizationDetailService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/").authenticated()
+                //.antMatchers("/org").hasAnyAuthority("ADMIN")
+                .anyRequest().authenticated();
+
+        http.formLogin(withDefaults());
+//                .loginPage("/login")
+//                //.loginProcessingUrl("/myuserlogin")
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login?error")
+//                .permitAll();
+
+        //        <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+    }
 }
