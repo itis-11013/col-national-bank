0bb057f92a72ec036eeaa6ef97f27b929be1ff38
Kevinbarrero <kevinbar98@gmail.com>
2022-05-14 13:35:43 +0300

    account repository query's

--- sem_col/src/main/java/ru/itis/sem_col/repositories/AccountRepository.java
+++ sem_col/src/main/java/ru/itis/sem_col/repositories/AccountRepository.java
@@ -1,12 +1,15 @@
 package ru.itis.sem_col.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.itis.sem_col.models.Account;
 import ru.itis.sem_col.models.Organization;
 
 import java.util.Optional;
 
 public interface AccountRepository extends CrudRepository <Account, Long> {
-    Account findByOrganization(Organization organization);
-    Optional<Account> findById(Long id);
+    @Query("select r from Organization r where r.id = :id ")
+    Account findByOrganizationId(@Param("id") Long id);
+
 }
