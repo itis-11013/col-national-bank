c63297158418deb4eef994d201b9fc2635821c40
Kevinbarrero <kevinbar98@gmail.com>
2022-05-19 17:20:59 +0300

    Added css to create an organization

--- sem_col/src/test/java/ru/itis/sem_col/bankApi/PostCreateBank.java
+++ sem_col/src/test/java/ru/itis/sem_col/bankApi/PostCreateBank.java
@@ -1,4 +1,4 @@
-package ru.itis.sem_col;
+package ru.itis.sem_col.bankApi;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
--- sem_col/src/test/java/ru/itis/sem_col/organizationApi/GetOrganizationTest.java
+++ sem_col/src/test/java/ru/itis/sem_col/organizationApi/GetOrganizationTest.java
@@ -1,20 +1,16 @@
-package ru.itis.sem_col;
+package ru.itis.sem_col.organizationApi;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-public class RequestBankTest {
-
-
+public class GetOrganizationTest {
     public static void main(String[] args) throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
         String fooResourceUrl
-                = "http://188.93.211.195/central/bank";
+                = "http://188.93.211.195/central/organization";
         ResponseEntity<String> response
                 = restTemplate.getForEntity(fooResourceUrl + "", String.class);
         //System.out.println(response);
