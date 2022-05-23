d747b9f62f32dcded4eb71bad8b34a9905516bc4
Kevinbarrero <kevinbar98@gmail.com>
2022-05-23 15:20:20 +0300

    api service [POST, REQUEST] for Organization

--- sem_col/src/main/java/ru/itis/sem_col/services/IOrgService.java
+++ sem_col/src/main/java/ru/itis/sem_col/services/IOrgService.java
@@ -1,8 +1,9 @@
 package ru.itis.sem_col.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import ru.itis.sem_col.controllers.dto.RegisterOrganizationDto;
 import ru.itis.sem_col.models.Organization;
 
 public interface IOrgService {
-    Organization registerNewOrganization(RegisterOrganizationDto organizationDto);
+    Organization registerNewOrganization(RegisterOrganizationDto organizationDto) throws JsonProcessingException;
 }
--- sem_col/src/main/java/ru/itis/sem_col/services/OrganizationCreateService.java
+++ sem_col/src/main/java/ru/itis/sem_col/services/OrganizationCreateService.java
@@ -1,7 +1,15 @@
 package ru.itis.sem_col.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import ru.itis.sem_col.controllers.dto.RegisterOrganizationDto;
 import ru.itis.sem_col.models.Country;
 import ru.itis.sem_col.models.Organization;
@@ -10,6 +18,7 @@ import ru.itis.sem_col.repositories.OrganizationRepository;
 
 import javax.transaction.Transactional;
 import java.util.UUID;
+
 
 @Service
 @Transactional
@@ -20,8 +29,29 @@ public class OrganizationCreateService implements IOrgService{
     private CountryRepository countryRepository;
 
     @Override
-    public Organization registerNewOrganization(RegisterOrganizationDto organizationDto) {
-        UUID uuid = UUID.randomUUID();
+    public Organization registerNewOrganization(RegisterOrganizationDto organizationDto) throws JsonProcessingException {
+        String url = "http://188.93.211.195/central/organization";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        JSONObject map = new JSONObject();
+        map.put("name", organizationDto.getName());
+        map.put("country", "co");
+        map.put("address", organizationDto.getAddress());
+        map.put("url", "--");
+        System.out.println(map);
+
+        HttpEntity<String> request = new HttpEntity<String>(map.toString(), headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String personResultAsJsonStr =
+                restTemplate.postForObject(url, request, String.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode root = objectMapper.readTree(personResultAsJsonStr);
+        JsonNode innerId = root.path("innerid");
+        System.out.println("InnerID New Org in Server: "+ innerId);
+
+        /////
+        UUID uuid = UUID.fromString(innerId.asText());
         Country country = countryRepository.findByCode("co");
         Organization organization = new Organization();
         organization.setName(organizationDto.getName());
